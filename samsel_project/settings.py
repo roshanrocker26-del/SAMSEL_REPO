8d97ae2fef13315fa0a61338e371c900b3ba2998
indhumurugan593-gif <indhumurugan593@gmail.com>
2026-02-23 13:34:23 +0530

    updated superadmin

--- samsel_project/settings.py
+++ samsel_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'samsel_website',
 ]
 
 MIDDLEWARE = [
@@ -89,8 +90,12 @@ WSGI_APPLICATION = 'samsel_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'samweb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',  # Leave empty if no password, or update as needed
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
